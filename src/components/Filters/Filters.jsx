6a6fc24b20608baf625af99677f955e05acd4f1d
Nathan Wood <nathanwood@Nathans-Mac-mini.local>
2022-06-21 12:44:33 -0400

    added carCount filter

--- src/components/Filters/Filters.jsx
+++ src/components/Filters/Filters.jsx
@@ -1,45 +1,52 @@
 import React from 'react';
 import FilterButton from './components/FilterButton';
 
-function Filters({setTrainFilter}) {
+function Filters({trainFilter, setTrainFilter, carCounts}) {
   return (
-    <div style={{
-      display: 'flex',
-      flexDirection: 'column',
-      margin: '2rem 1rem',
-    }}>
+    <div style={{margin: '1rem 1rem 2rem'}}>
       <h2>Filter Trains By:</h2>
       <div style={{
         display: 'flex',
-        justifyContent: 'space-between'
+        justifyContent: 'space-between',
+        marginBottom: '1rem',
       }}>
         <div>
           <h4>Train Line:</h4>
           <div>
-            <FilterButton text="Red" filterColumn={'LineCode'} filterValue={'RD'} setTrainFilter={setTrainFilter} />
-            <FilterButton text="Blue" filterColumn={'LineCode'} filterValue={'BL'} setTrainFilter={setTrainFilter} />
-            <FilterButton text="Yellow" filterColumn={'LineCode'} filterValue={'YL'} setTrainFilter={setTrainFilter} />
-            <FilterButton text="Orange" filterColumn={'LineCode'} filterValue={'OR'} setTrainFilter={setTrainFilter} />
-            <FilterButton text="Grey" filterColumn={'LineCode'} filterValue={'GR'} setTrainFilter={setTrainFilter} />
-            <FilterButton text="Silver" filterColumn={'LineCode'} filterValue={'SL'} setTrainFilter={setTrainFilter} />
+            <FilterButton text="Red" filterColumn={'LineCode'} filterValue={'RD'} trainFilter={trainFilter} setTrainFilter={setTrainFilter} />
+            <FilterButton text="Blue" filterColumn={'LineCode'} filterValue={'BL'} trainFilter={trainFilter} setTrainFilter={setTrainFilter} />
+            <FilterButton text="Yellow" filterColumn={'LineCode'} filterValue={'YL'} trainFilter={trainFilter} setTrainFilter={setTrainFilter} />
+            <FilterButton text="Orange" filterColumn={'LineCode'} filterValue={'OR'} trainFilter={trainFilter} setTrainFilter={setTrainFilter} />
+            <FilterButton text="Green" filterColumn={'LineCode'} filterValue={'GR'} trainFilter={trainFilter} setTrainFilter={setTrainFilter} />
+            <FilterButton text="Silver" filterColumn={'LineCode'} filterValue={'SV'} trainFilter={trainFilter} setTrainFilter={setTrainFilter} />
           </div>
         </div>
         <div>
           <h4>Service Type:</h4>
           <div>
-            <FilterButton text="Normal" filterColumn={'ServiceType'} filterValue={'Normal'} setTrainFilter={setTrainFilter} />
-            <FilterButton text="No Passengers" filterColumn={'ServiceType'} filterValue={'NoPassengers'} setTrainFilter={setTrainFilter} />
-            <FilterButton text="Special" filterColumn={'ServiceType'} filterValue={'Special'} setTrainFilter={setTrainFilter} />
-            <FilterButton text="Unknown" filterColumn={'ServiceType'} filterValue={'Unknown'} setTrainFilter={setTrainFilter} />
+            <FilterButton text="Normal" filterColumn={'ServiceType'} filterValue={'Normal'} trainFilter={trainFilter} setTrainFilter={setTrainFilter} />
+            <FilterButton text="No Passengers" filterColumn={'ServiceType'} filterValue={'NoPassengers'} trainFilter={trainFilter} setTrainFilter={setTrainFilter} />
+            <FilterButton text="Special" filterColumn={'ServiceType'} filterValue={'Special'} trainFilter={trainFilter} setTrainFilter={setTrainFilter} />
+            <FilterButton text="Unknown" filterColumn={'ServiceType'} filterValue={'Unknown'} trainFilter={trainFilter} setTrainFilter={setTrainFilter} />
           </div>
         </div>
         <div>
           <h4>Number of Cars:</h4>
+          <select
+            onChange={(e) => setTrainFilter({
+              column: 'CarCount',
+              value: parseInt(e.target.value),
+            })}
+          >
+            {carCounts?.map((value) => (
+              <option key={value} value={value}>{value}</option>
+            ))}
+          </select>
         </div>
-        <div>
-          <h4>Clear Filter:</h4>
-          <FilterButton text="Clear" filterColumn={null} filterValue={null} setTrainFilter={setTrainFilter} />
-        </div>
+      </div>
+      <div>
+        <h4>Clear Filter:</h4>
+        <FilterButton text="Clear" filterColumn={null} filterValue={null} trainFilter={trainFilter} setTrainFilter={setTrainFilter} />
       </div>
     </div>
   );
